e27ead9b11a097f46bf2f610c304fc14e8b1ce9c
Abhishek Gupta <guptaabhi920@gmail.com>
2023-05-17 00:55:36 +0530

    [day1]

---
+++ loops.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main(){
+    int age;
+    printf("Enter your age: ");
+    scanf("%d",&age);
+    // if(age<18)
+    //     printf("You cannot drive");
+    // else if(age>=18 && age<=24)
+    //     printf("you can drive but be careful");
+    // else
+    //     printf("you can drive as fast as possible");
+    // int price=age>18?90000:65000;
+    // printf("%d",price);
+    int index=0;
+    while(index<10){
+        printf("%d\n",index);
+        index++;
+    }
+
+    
+}
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,4 @@
+int n1,n2;
+    scanf("%d\n",&n1);
+    scanf("%d\n",&n2);
+    printf("divide of n1 and n2 is: %f\n",(float)n1/n2);
